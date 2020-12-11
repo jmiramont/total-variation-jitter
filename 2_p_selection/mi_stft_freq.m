7fff9327e57eae8f3e3bcb9f9dfba4628a2fee35
J.M <62579770+jmiramont@users.noreply.github.com>
2020-12-11 11:18:51 -0300

    Update mi_stft_freq.m

--- 2_p_selection/mi_stft_freq.m
+++ 2_p_selection/mi_stft_freq.m
@@ -1,5 +1,5 @@
 function STFT = mi_stft_freq(s,gamma,sigma,ft,bt)
-% sstn : computes the STFT of a signal
+% Computes the STFT of a signal
 %   Uses a Gaussian window.
 %
 % INPUTS:
@@ -24,37 +24,17 @@ n = length(s);
 neta = length(ft);
 s = s(:);
 
-% Window definition
-f = 0:n-1;
-S = fft(s);
+
+f = 0:n-1; % Frequency partition.
+S = fft(s); % Fourier transform of the function.
 
 % Initialization
 STFT = zeros(neta,n);
 
-%Tc = 6/(sigma*sqrt(2*pi));
-%myhann=@(t,T)0.5*(1+cos(2*pi*t/T)).*indicatriz(t,T);
-
-
 for j = ft-1
-    
-    g_hat = exp(-sigma^2*pi*(f-j).^2);
-    %g_hat = myhann((f-j),Tc);
-    STFT(j+1,:) = ifft(S'.*conj(g_hat));
+    g_hat = exp(-sigma^2*pi*(f-j).^2); % Fourier transform of the analysis window.
+    STFT(j+1,:) = ifft(S'.*conj(g_hat)); % Filtering in frequency domain (fill the STFT row-wise)
 end
 
 
 end
-
-function chi = indicatriz(t,T)
-chi=zeros(1,length(t));
-chi(t>=-0.5*T & t<=0.5*T)=1;
-end
-
-
-
-
-
-
-
-
-
